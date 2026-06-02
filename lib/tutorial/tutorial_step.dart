b6603c913d39ec70e8541f21d02a1942f5a076c0
Hex-Umbra <g.p.dacosta@hotmail.com>
2026-06-02 14:36:18 +0200

    feat(tutorial): define TutorialStep and TutorialStepType enum

---
+++ lib/tutorial/tutorial_step.dart
@@ -0,0 +1,33 @@
+class TutorialStep {
+  final String titleFr;
+  final String titleEn;
+  final String bodyFr;
+  final String bodyEn;
+  final TutorialStepType type;
+  final String? highlightZone;
+
+  const TutorialStep({
+    required this.titleFr,
+    required this.titleEn,
+    required this.bodyFr,
+    required this.bodyEn,
+    required this.type,
+    this.highlightZone,
+  });
+}
+
+enum TutorialStepType {
+  welcome,
+  map,
+  nodeTypes,
+  combatOverview,
+  cards,
+  playCard,
+  armorDamage,
+  elements,
+  enemies,
+  merge,
+  xp,
+  draft,
+  relics,
+}
