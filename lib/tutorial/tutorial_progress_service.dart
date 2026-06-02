252bf14a723b56f80bce1dfef879eecf6104b438
Hex-Umbra <g.p.dacosta@hotmail.com>
2026-06-02 14:38:27 +0200

    feat(tutorial): implement TutorialProgressService using SharedPreferences

---
+++ lib/tutorial/tutorial_progress_service.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TutorialProgressService {
+  static const String _tutorialCompletedKey = 'tutorial_completed';
+
+  static Future<bool> hasCompletedTutorial() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_tutorialCompletedKey) ?? false;
+  }
+
+  static Future<void> markTutorialCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_tutorialCompletedKey, true);
+  }
+
+  static Future<void> resetTutorial() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_tutorialCompletedKey, false);
+  }
+}
